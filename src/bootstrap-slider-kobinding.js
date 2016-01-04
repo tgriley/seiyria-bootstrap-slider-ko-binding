08dff5a0155b21438b2bd61a5a08f47a80d952eb
Tom Riley <tgriley1@gmail.com>
2016-01-04 08:39:35 +0000

    WIP

---
+++ src/bootstrap-slider-kobinding.js
@@ -0,0 +1,27 @@
+ko.bindingHandlers.slider = {
+    init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+        var params = valueAccessor();
+        var valueObservable = params['value'];
+
+        for (var property in params) {
+            if (ko.isObservable(params[property]))
+                params[property] = ko.unwrap(params[property]);
+        }
+
+        $(element).slider(params);
+
+        $(element).on('change', function (ev) {
+            valueObservable(ev.newValue);
+        });
+    },
+    update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+        var modelValue = valueAccessor();
+        var valueObservable;
+        if (ko.isObservable(modelValue))
+            valueObservable = modelValue;
+        else
+            valueObservable = modelValue['value'];
+
+        $(element).slider('setValue', valueObservable());
+    }
+};
